e32170cc12c569f7b22ea33206e761e7de64a22d
Yoni <cyoni10@gmail.com>
2020-08-06 11:56:26 +0300

    finish the game

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
 import random
+import math
 
-playing = True
-
-number = random.randrange(1000, 9999, 1)
+playing = False
 
 
-success = []
+def convertNumberToArray(numberToGuess):
+    number_array = []
+    for x in range(4):
+        number_array.append(numberToGuess % 10)
+        numberToGuess = math.floor(numberToGuess / 10)
+    number_array.reverse()
+    return number_array
 
-for x in range(4):
-    success.append("*")
 
-while playing:
-    print("Guess the number ", success)
-    user_input = input("Guess the number: ")
-    print("hi " + user_input)
+def giveNumber():
+    return random.randrange(1000, 9999, 1)
+
+
+def printStatus(correct, wrong):
+    print("bulls: ", correct, "cows: ", wrong)
+
+
+def countCorrectAndWrongAnswer(input_array, number_array, correct, wrong):
+    for i in range(4):
+        if (input_array[i] == number_array[i]):
+            correct += 1
+        else:
+            wrong += 1
+    return [correct, wrong]
+
+
+def startGame():
+    global playing
+    playing = True
+    numberToGuess = giveNumber()
+    print("ans ", numberToGuess)
+    number_array = convertNumberToArray(numberToGuess)
+    game(numberToGuess, number_array)
+
+
+def game(numberToGuess, number_array):
+    global playing
+    while playing:
+        user_input = input("Guess the number: ")
+        input_array = convertNumberToArray(int(user_input))
+
+        correct = 0
+        wrong = 0
+
+        status = countCorrectAndWrongAnswer(input_array, number_array, correct, wrong)
+        correct = status[0]
+        wrong = status[1]
+        printStatus(correct, wrong)
+
+        if (correct == 4):
+            print("You win!")
+            playing = False
+
+
+startGame()
